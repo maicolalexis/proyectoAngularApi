772ff3475d158ad42bac128ce4434c34f494690e
maicolalexis <maicolalexis222@gmail.com>
2022-12-14 15:27:36 -0500

    hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './products/crud/create/create.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'productos', component: ProductsComponent},
+{path: 'productos/create', component: CreateComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsComponent } from './products/products.component';
+import { NavbarComponent } from './apiGithub/navbar/navbar.component';
+import { SearchPersonComponent } from './apiGithub/search-person/search-person.component';
+import { ShowDatePersonComponent } from './apiGithub/show-date-person/show-date-person.component';
+import { ComponentParentComponent } from './apiGithub/component-parent/component-parent.component'
+import { FormsModule } from '@angular/forms';
+import { CreateComponent } from './products/crud/create/create.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NavbarComponent,
+    SearchPersonComponent,
+    ShowDatePersonComponent,
+    ComponentParentComponent,
+    CreateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
